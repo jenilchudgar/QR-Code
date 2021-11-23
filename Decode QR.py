e0085c0e9848f4da049889bb39f27f996ad3a2b1
Jenil Chudgar <jenilchudgarfamily@gmail.com>
2021-11-23 17:24:35 +0530

    Added Image!

--- Decode QR.py
+++ Decode QR.py
@@ -2,7 +2,7 @@ import cv2
 import os
 
 detector = cv2.QRCodeDetector()
-val,_ ,_ = detector.detectAndDecode(cv2.imread("\\img"+input("File Name: ")+".png"))
+val,_ ,_ = detector.detectAndDecode(cv2.imread("img"+"\\"+input("File Name: ")+".png"))
 print("Value:",val)
 
 os.system('git commit -a -m "Added Image!" ')
